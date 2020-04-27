f1f8ce9f8a61feb64aa4c1ba4165c15b48fb75fe
ivan006 <ivan.copeland2015@gmail.com>
2020-04-27 12:09:21 +0200

    1

--- 3/conf/_base.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @version 1.7
- * @link https://github.com/mc007
- * @author mc007 mc007@pearls-media.com
- * @license : GPL v2. http://www.gnu.org/licenses/gpl-2.0.html
- */
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reader', function () {
+  return view('reader');
+});
